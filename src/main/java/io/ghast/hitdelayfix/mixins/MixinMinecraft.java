580b899a2e91724396882929b89cce1277c60064
standayweb <stan@standayweb.com>
2020-08-30 16:09:59 +0100

    initial commit

---
+++ src/main/java/io/ghast/hitdelayfix/mixins/MixinMinecraft.java
@@ -0,0 +1,22 @@
+package io.ghast.hitdelayfix.mixins;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+
+    @Shadow private int leftClickCounter;
+
+    @Inject(method = "clickMouse", at = @At("HEAD"))
+    private void clickMouseAfter(final CallbackInfo ci) {
+        leftClickCounter = 0;
+    }
+}
+
+
